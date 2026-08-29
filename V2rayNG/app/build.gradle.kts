5354b5ae4a53e71dd8ff0abe89af9a5a168bd235
ECLIPSE S.Rom <pomah7878@gmail.com>
2026-08-29 15:33:51 +0300

    Тест статического DownloadCompleteReceiver, versionName 1.13.13

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 745
-        versionName = "1.13.12"
+        versionName = "1.13.13"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
