efc410475b3eaba48e4de38def26c26103458e6b
ECLIPSE S.Rom <pomah7878@gmail.com>
2026-08-30 14:41:29 +0300

    Редизайн шаг 1/4: фиолетовый градиентный фон, versionName 1.17.0

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 745
-        versionName = "1.16.3"
+        versionName = "1.17.0"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
