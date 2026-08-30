f430efcfdc45245042e1c0171243d7657035dacd
ECLIPSE S.Rom <pomah7878@gmail.com>
2026-08-30 15:01:56 +0300

    Возврат к чёрно-золотому фону вместо фиолетового, versionName 1.17.1

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 24
         targetSdk = 37
         versionCode = 745
-        versionName = "1.17.0"
+        versionName = "1.17.1"
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
         splits {
